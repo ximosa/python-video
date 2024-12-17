2314b078a3e84001044405c7dba9b87b11d9443c
Joaquin Saez <ximosa@gmail.com>
2024-12-18 00:23:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -261,19 +261,19 @@ def get_youtube_creds():
       if not creds:
         try:
           # El flujo debe estar en formato "web" y las redirect uris deben de concordar
-            flow = google_auth_oauthlib.flow.Flow.from_client_config(
-                {
-                   "web":{
-                        "client_id": client_id,
-                        "project_id": st.secrets["youtube_credentials"]["project_id"],
-                        "auth_uri": auth_uri,
-                        "token_uri": token_uri,
-                        "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
-                        "client_secret": client_secret,
-                        "redirect_uris": redirect_uris
-                        }
-                  },
-                scopes = SCOPES)
+             flow = google_auth_oauthlib.flow.Flow.from_client_config(
+      {
+        "web":{
+            "client_id": client_id,
+            "project_id": st.secrets["youtube_credentials"]["project_id"],
+            "auth_uri": auth_uri,
+            "token_uri": token_uri,
+            "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
+            "client_secret": client_secret,
+            "redirect_uris": redirect_uris
+        }
+     },
+      scopes = SCOPES)
             
             auth_url, _ = flow.authorization_url(prompt='consent')
             st.session_state['auth_url'] = auth_url
